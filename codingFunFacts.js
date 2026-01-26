ecdaa8859f944b6b124acf4571da173914ab5d47
DGeorge17 <georgeroblescoronel02@gmail.com>
2026-01-25 16:38:49 -0800

    Create codingFunFacts.js

---
+++ codingFunFacts.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+// The bot's name, where it's from and it's favorite coding language
+let botName = 'Wall-e'
+let botLocation = 'Mars'
+let favoriteLanguage = 'Spanish'
+
+console.log("My name is " + botName + ' and I live on ' + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// First assignment and log
+let codingFact = 'FOREVERRR ' + favoriteLanguage + '.';
+console.log(codingFact); 
+
+// Second assignment and log
+codingFact = favoriteLanguage + " is so interesting and cool.";
+console.log(codingFact);
+
+// Third assignment and log
+codingFact = 'Peru and all of America speak ' + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + ' from ' + botLocation + ".");
