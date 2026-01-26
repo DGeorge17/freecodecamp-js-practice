485dc0ac3aa82e6e02b4c48a7a892dd8e39f0b95
DGeorge17 <georgeroblescoronel02@gmail.com>
2026-01-25 18:20:55 -0800

    Build-a- Sentence-Maker.js

--- Build-a- Sentence-Maker.js
+++ Build-a- Sentence-Maker.js
@@ -1,20 +1,21 @@
-let adjective = "big"
-let noun = 'dolphin'
-let verb = 'played'
-let place = 'sea'
-let adjective2 = 'relaxing'
-let noun2 = 'fish'
-const firstStory = "Once upon a time, there was a " + adjective + noun + "who loved to eat " + noun2 + '.' + "The " + noun + " lived in a " + place + ' and had ' + adjective2 + ' nostrils that blew fire when it was ' + verb + '.';
+let adjective = "big";
+let noun = "dolphin";
+let verb = "played";
+let place = "sea";
+let adjective2 = "relaxing";
+let noun2 = "fish";
 
-console.log("First story:" + firstStory);
+let firstStory ="Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
 
-//This is the 2nd assignment and log
-adjective = 'fantastic'
-noun = 'cat'
-verb = 'ran'
-place = 'shelter for animals'
-adjective2 = 'strong'
-noun2 = 'treats' 
-const secondStory = "Once upon a time, there was a " + adjective + noun + "who loved to eat " + noun2 + '.' + "The " + noun + " lived in a " + place + ' and had ' + adjective2 + ' nostrils that blew fire when it was ' + verb + '.';
+console.log("First story: " + firstStory);
 
-console.log("Second Story:" + secondStory);
+adjective = "fantastic";
+noun = "cat";
+verb = "ran";
+place = "shelter for animals";
+adjective2 = "strong";
+noun2 = "treats";
+
+let secondStory = "Once upon a time, there was a(n) " +adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
