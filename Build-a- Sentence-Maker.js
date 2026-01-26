b99df4547d8a07811936bad92b6c3a10b8217844
DGeorge17 <georgeroblescoronel02@gmail.com>
2026-01-25 17:39:37 -0800

    Build-a- Sentence-Maker.js

---
+++ Build-a- Sentence-Maker.js
@@ -0,0 +1,20 @@
+let adjective = "big"
+let noun = 'dolphin'
+let verb = 'played'
+let place = 'sea'
+let adjective2 = 'relaxing'
+let noun2 = 'fish'
+const firstStory = "Once upon a time, there was a " + adjective + noun + "who loved to eat " + noun2 + '.' + "The " + noun + " lived in a " + place + ' and had ' + adjective2 + ' nostrils that blew fire when it was ' + verb + '.';
+
+console.log("First story:" + firstStory);
+
+//This is the 2nd assignment and log
+adjective = 'fantastic'
+noun = 'cat'
+verb = 'ran'
+place = 'shelter for animals'
+adjective2 = 'strong'
+noun2 = 'treats' 
+const secondStory = "Once upon a time, there was a " + adjective + noun + "who loved to eat " + noun2 + '.' + "The " + noun + " lived in a " + place + ' and had ' + adjective2 + ' nostrils that blew fire when it was ' + verb + '.';
+
+console.log("Second Story:" + secondStory);
